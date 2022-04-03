021b19562a1f5555f47d6de829f817bd08fa9578
MusaPolka <torneo1508@gmail.com>
2022-04-03 11:37:09 +0600

    Disposable task added

---
+++ week2/DisposableTask/DisposableTask/Customer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisposableTask
+{
+    public class Customer : IDisposable
+    {
+        private StringReader _reader;
+        bool disposed;
+
+        public Customer(string s)
+        {
+            this._reader = new StringReader(s);
+        }
+
+        public void Print()
+        {
+            if (disposed)
+            {
+                Console.WriteLine("Reference has already disposed");
+            }
+            else
+            {
+                Console.WriteLine(_reader.ReadToEnd());
+            } 
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool dispose)
+        {
+            if (disposed)
+            {
+                return;
+            }
+
+            if (dispose)
+            {
+                _reader.Dispose();
+            }
+
+            disposed = true;
+        }
+
+        ~Customer()
+        {
+            _reader.Dispose();
+        }
+    }
+}
---
+++ week2/DisposableTask/DisposableTask/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DisposableTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer("Musa");
+
+            customer.Print();
+
+            customer.Dispose();
+
+            customer.Print();
+        }
+    }
+}
