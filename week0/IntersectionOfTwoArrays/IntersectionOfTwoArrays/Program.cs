4583d754b86b6a27c9fb38e4637e90f0d83e54de
MusaPolka <torneo1508@gmail.com>
2022-04-13 15:27:40 +0600

    week 0 tasks

---
+++ week0/IntersectionOfTwoArrays/IntersectionOfTwoArrays/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IntersectionOfTwoArrays
+{
+    internal class Program
+    {
+        public static int[] Intersect(int[] nums1, int[] nums2)
+        {
+            var returnList = new List<int>();
+            var list = new List<int>();
+
+            foreach (var item in nums2)
+            {
+                list.Add(item);
+            }
+
+            for (int i = 0; i < nums1.Length; i++)
+            {
+                for (int j = 0; j < list.Count; j++)
+                {
+                    if (nums1[i] == list[j])
+                    {
+                        returnList.Add(list[j]);
+                        list.RemoveAt(j);
+                        break;
+                    }
+                }
+            }
+
+            return returnList.ToArray();
+        }
+        static void Main(string[] args)
+        {
+            int[] nums1 = new int[] { 1, 2, 2, 1 };
+            int[] nums2 = new int[] { 1, 2 };
+
+            var newArr = Intersect(nums1, nums2);
+
+            foreach (var item in newArr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ week0/RotateArray/RotateArray/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace RotateArray
+{
+    internal class Program
+    {
+        public static int[] Rotate(int[] nums, int k)
+        {
+            int [] nums2 = new int[nums.Length];
+            int rotateFrom = nums.Length - k;
+            int index = 0;
+
+            for (int i = 0; i < k; i++)
+            {
+                nums2[i] = nums[index + rotateFrom];
+                index++;
+            }
+
+            for (int i = 0; i < rotateFrom; i++)
+            {
+                nums2[index] = nums[i];
+                index++;
+            }
+
+            return nums2;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+
+            var newArr = Rotate(nums, 3);
+
+            foreach (var item in newArr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
