af69846a42b9b6552edd89922129dabf48a6736c
MusaPolka <torneo1508@gmail.com>
2022-04-16 22:21:33 +0600

    DownloaderTask - not working demo

---
+++ week3/DownloaderTask/DownloaderTask/Downloader.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DownloaderTask
+{
+    
+    public class Downloader
+    {
+        public string DownloadString(string url)
+        {
+            using var client = new WebClient();
+
+            var responseString = client.DownloadString(url);
+            
+
+            return responseString;
+        }
+
+        public List<Photo> GetPhotosModel()
+        {
+            var responseString = DownloadString("https://jsonplaceholder.typicode.com/photos");
+
+            dynamic json = JObject.Parse(responseString);
+
+            List<Photo> photos = new List<Photo>();
+
+            foreach (var item in json)
+            {
+                Photo photo = new Photo()
+                {
+                    albumId = item.albumId,
+                    id = item.id,
+                    title = item.title,
+                    url = item.url,
+                    thumbnailUrl = item.thumbnailUrl
+                };
+                photos.Add(photo);
+            }
+
+            return photos;
+        }
+
+        public void DownloadPhotos()
+        {
+
+        }
+    }
+}
---
+++ week3/DownloaderTask/DownloaderTask/Program.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+
+namespace DownloaderTask
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+
+            var client = new WebClient();
+            var responseString = client.DownloadString("https://jsonplaceholder.typicode.com/photos");
+
+
+            //Console.WriteLine(responseString);
+        }
+    }
+}
